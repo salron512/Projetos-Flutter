eb07b800d0f81ede27ab49946eea3f4d391211c2
salron512 <73781657+salron512@users.noreply.github.com>
2020-12-09 12:08:34 -0400

    Update main.dart

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -2,11 +2,42 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 void main() async {
- WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
- Firestore db =  Firestore.instance;
+  Firestore db = Firestore.instance;
+
+  // faz consulta no banco de dados a cada alteracao no banco de forma automatica
+  db.collection("usuarios").snapshots().listen((snapshot) {
+    for (DocumentSnapshot item in snapshot.documents) {
+      var dados = item.data;
+      print("nome: " +
+          dados["nome"].toString() +
+          " idade:  " +
+          dados["idade"].toString());
+    }
+  });
+
+/*
+//recupera dados do banco de dados
+QuerySnapshot querySnapshot = await db.collection("usuarios").getDocuments();
+ //print("dados usuarios: " + querySnapshot.documents.toString());
+for(DocumentSnapshot item in querySnapshot.documents){
+  var dados = item.data;
+ print("dados usuarios: " + dados["nome"] + " idade: " + dados["idade"] );
+}
+*/
+
+//recupera dados do banco de dados
+  /*
+DocumentSnapshot snapshot = await db.collection("usuarios").document("001").get();
+var dados = snapshot.data;
+print("dados: " + dados["nome"] + " idade: " + dados["idade"]);
+*/
+
+  // deleta registros no banco de dados
+//db.collection("usuarios").document("003").delete();
+
 /*
   db.collection("usuarios")
   .document("002")
@@ -20,7 +51,7 @@ void main() async {
     "titulo" : "Onde de calor em MT",
     "descricao" : "texto exemplo..."
   });
-     print("item salva: " + ref.documentID);
+     print("item salvo: " + ref.documentID);
 
 
  db.collection("noticias")
