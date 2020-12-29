2652ebf0fee1a7b3032c39ef70c63864a21b60a9
salron512 <73781657+salron512@users.noreply.github.com>
2020-12-28 21:17:17 -0400

    commit

--- maps_flutter/lib/Home.dart
+++ maps_flutter/lib/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,6 +11,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Set<Marker> _marcadadores = {};
   Set<Polygon> _polygons = {};
+  Set<Polyline> _polyline = {};
 
   Completer<GoogleMapController> _controller = Completer();
 
@@ -23,10 +25,11 @@ class _HomeState extends State<Home> {
     googleMapController.animateCamera(CameraUpdate.newCameraPosition(
         CameraPosition(
             target: LatLng(-15.579162, -57.913824),
-            zoom: 16,
+            zoom: 18,
             tilt: 0,
             bearing: 0)));
   }
+  /*
 
   _carregarMarcadores() {
     Set<Marker> _marcadadoresLocal = {};
@@ -55,21 +58,21 @@ class _HomeState extends State<Home> {
     });
     Set<Polygon> listaPolygons = {};
     Polygon polygon1 = Polygon(
-      polygonId: PolygonId("polygo1"),
-      fillColor: Colors.transparent,
-      strokeColor: Colors.red,
-      strokeWidth: 5,
-      points: [
-        LatLng(-15.580051, -57.914562),
-        LatLng(-15.580060, -57.914791),
-        LatLng(-15.580463, -57.914732),
-        LatLng(-15.580398, -57.914498),
-      ],
-      consumeTapEvents: true,
-      onTap: () {
-        print("clicando na área");
-      },
-    );
+        polygonId: PolygonId("polygo1"),
+        fillColor: Colors.transparent,
+        strokeColor: Colors.red,
+        strokeWidth: 5,
+        points: [
+          LatLng(-15.580051, -57.914562),
+          LatLng(-15.580060, -57.914791),
+          LatLng(-15.580463, -57.914732),
+          LatLng(-15.580398, -57.914498),
+        ],
+        consumeTapEvents: true,
+        onTap: () {
+          print("clicando na área");
+        },
+        zIndex: 0);
     listaPolygons.add(polygon1);
 
     setState(() {
@@ -78,31 +81,64 @@ class _HomeState extends State<Home> {
 
     Set<Polygon> listaPolygon2 = {};
     Polygon polygon2 = Polygon(
-      polygonId: PolygonId("polygo2"),
-      fillColor: Colors.purple,
-      strokeColor: Colors.orange,
-      strokeWidth: 2,
-      points: [
-        LatLng(-15.580222, -57.914420),
-        LatLng(-15.580269, -57.914838),
-        LatLng(-15.580473, -57.914583),
-      ],
-      consumeTapEvents: true,
-      onTap: () {
-        print("clicando na área");
-      },
-    );
-    listaPolygons.add(polygon1);
+        polygonId: PolygonId("polygo2"),
+        fillColor: Colors.purple,
+        strokeColor: Colors.orange,
+        strokeWidth: 2,
+        points: [
+          LatLng(-15.580222, -57.914420),
+          LatLng(-15.580269, -57.914838),
+          LatLng(-15.580473, -57.914583),
+        ],
+        consumeTapEvents: true,
+        onTap: () {
+          print("clicando na área");
+        },
+        zIndex: 1);
+    listaPolygons.add(polygon2);
 
     setState(() {
       _polygons = listaPolygon2;
     });
+
+    Set<Polyline> listaPolylines = {};
+
+    Polyline polyline = Polyline(
+        polylineId: PolylineId("polyline"),
+        color: Colors.red,
+        width: 10,
+        startCap: Cap.roundCap,
+        endCap: Cap.roundCap,
+        jointType: JointType.bevel,
+        points: [
+          LatLng(-15.580051, -57.914562),
+          LatLng(-15.580060, -57.914791),
+          LatLng(-15.580463, -57.914732),
+        ],
+        consumeTapEvents: true,
+        onTap: () {
+          print("clicando na polyline");
+        });
+    listaPolylines.add(polyline);
+
+    setState(() {
+      _polyline = listaPolylines;
+    });
+  }
+  */
+
+  _recuperaLocalizacaoUSuario() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    print("Minha localização" + position.toString());
   }
 
   @override
   void initState() {
     super.initState();
-    _carregarMarcadores();
+    // _carregarMarcadores();
+    _recuperaLocalizacaoUSuario();
   }
 
   @override
@@ -122,13 +158,14 @@ class _HomeState extends State<Home> {
         child: GoogleMap(
           mapType: MapType.hybrid,
           initialCameraPosition: CameraPosition(
-              target: LatLng(-15.611345, -57.919572),
+              target: LatLng(-15.580060, -57.914791),
               zoom: 17,
               tilt: 0,
               bearing: 30),
           onMapCreated: _onMapCreated,
           markers: _marcadadores,
           polygons: _polygons,
+          polylines: _polyline,
         ),
       ),
     );
