b9c105c4d6f14bfb0649198f8bbc501d385a9f1b
salron512 <73781657+salron512@users.noreply.github.com>
2021-04-01 18:17:58 -0400

    commit

--- projeto_aelton/lib/main.dart
+++ projeto_aelton/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:projeto_aelton/util/RouteGenerator.dart';
-
 import 'Login.dart';
 
 
@@ -14,6 +14,19 @@ final ThemeData temaPadrao = ThemeData(
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.init(
+      "98b0698b-f0a2-4843-be0a-fd358da3bda1",
+      iOSSettings: {
+        OSiOSSettings.autoPrompt: false,
+        OSiOSSettings.inAppLaunchUrl: false
+      }
+  );
+  OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  await OneSignal.shared.promptUserForPushNotificationPermission(fallbackToSettings: true);
   runApp(MaterialApp(
     home: Login() ,
     debugShowCheckedModeBanner: false,
