55d3a879f629b0e21f3d8f0eb330cc7b384d9193
ANDRÉ RICARDO VICENSOTTI <73781657+salron512@users.noreply.github.com>
2022-04-16 16:13:42 -0400

    commit

--- acha_eu/lib/main.dart
+++ acha_eu/lib/main.dart
@@ -4,15 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-/*
-final ThemeData temaPadrao = ThemeData(
-  primaryColor: Color(0xff37474f),
-  backgroundColor: Colors.grey,
-  colorScheme: ColorScheme.fromSwatch().copyWith(
-    
-    secondary: Color(0xff546e7a)),
-);
-*/
 final ThemeData temaPadrao = ThemeData(
   backgroundColor: Colors.grey,
   colorScheme: ColorScheme.fromSwatch().copyWith(
