d4f1ec35c1efc9daf990cf38cd4865f9a2c1d18a
ANDRÉ RICARDO VICENSOTTI <73781657+salron512@users.noreply.github.com>
2022-12-04 17:35:28 -0400

    commit

--- backend/lib/backend.dart
+++ backend/lib/backend.dart
@@ -5,6 +5,19 @@ import 'package:shelf_modular/shelf_modular.dart';
 Future<Handler> startShelfModular() async {
   var handler = Modular(module: AppModule(), middlewares: [
     logRequests(),
+    jsonResponse(),
   ]);
   return handler;
+}
+
+Middleware jsonResponse() {
+  return (handler) {
+    return (request) async {
+      var response = await handler(request);
+
+      response = response.change(
+          headers: {'content-type': 'application/json', ...response.headers});
+      return response;
+    };
+  };
 }
--- backend/lib/src/app_module.dart
+++ backend/lib/src/app_module.dart
@@ -1,22 +1,22 @@
 import 'package:backend/src/core/services/database/postgres/postgres_database.dart';
 import 'package:backend/src/core/services/database/remote_database.dart';
 import 'package:backend/src/core/services/dot_env/dot_env_services.dart';
+import 'package:backend/src/features/swagger/swagger_handler.dart';
 import 'package:backend/src/features/user/user_resource.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
 
 class AppModule extends Module {
-@override
-  List<Bind> get binds =>[
-    Bind.instance<DotEnvService>(DotEnvService()),
-    Bind.singleton<RemoteDatabase>((i) => PostgresDatabase(i())),
-  ];
-  
   @override
-  List<ModularRoute> get routes =>
-      [
+  List<Bind> get binds => [
+        Bind.instance<DotEnvService>(DotEnvService()),
+        Bind.singleton<RemoteDatabase>((i) => PostgresDatabase(i())),
+      ];
+
+  @override
+  List<ModularRoute> get routes => [
         Route.get('/', (Request request) => Response.ok('Inicial')),
-          Route.resource(UserResource()),
-          
+        Route.get('/documentation/**', swaggerHandler),
+        Route.resource(UserResource()),
       ];
 }
